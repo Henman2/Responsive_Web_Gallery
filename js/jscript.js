1bdf2bc9cf5c510240bab58712a66619be3f19ad
Henman2 <47754370+Henman2@users.noreply.github.com>
2023-09-12 19:14:43 +0000

    Add images dynamically

--- js/jscript.js
+++ js/jscript.js
@@ -1,14 +1,27 @@
- // Get the modal
- var modal = document.getElementById('myModal');
-         
- // Get the image and insert it inside the modal - use its "alt" text as a caption
- // var img = document.getElementById('car1');
- // select and image from class with name thumbnail
- var selectedImg = document.querySelectorAll('.thumbnail'); 
- var modalImg = document.getElementById("imgModal");
- // var captionText = document.getElementById("caption");
+//get array of all the images saved in gallery
+const numberOfImages = 9;
+const imgSources = [];
+for(let i = 1; i <= numberOfImages; i++){
+    imgSources.push(`gallery/car${i}.jpg`);
+}
 
- for(var i=0; i < selectedImg.length; i++) {
+ const imgContainer = document.querySelector(".flex-container");
+ //loop through and display
+ imgSources.forEach((src, index) =>{
+    const img = document.createElement("img");
+    img.classList.add("thumbnail");
+    img.src = src;
+    img.alt = `Ford Raptor ${index+1}`;
+    imgContainer.appendChild(img);
+ });
+
+ // Get the modal
+ var modal = document.getElementById('myModal');       
+ // select an image from class with name thumbnail
+ let selectedImg = document.querySelectorAll('.thumbnail'); 
+ let modalImg = document.getElementById("imgModal");
+ // var captionText = document.getElementById("caption");
+ for(let i=0; i < selectedImg.length; i++) {
      selectedImg[i].onclick = function(){
          modal.style.display = "block";
          modalImg.src = this.src;
